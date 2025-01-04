49da685ae93c8c162e4caa68ab0c6c752d0e99e3
“CaitlinYoung7” <caitlincnyoung@gmail.com>
2025-01-04 11:43:15 +0000

    Fully completed every ticket

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-import express from `express`
-//importing express module 
-
-
+import express from "express";
+//importing the express module
 const app = express();
+//creating the varibale of app assigning it the value of the express module
 
-//define the server 
-const port = 3000;
-//define the port the server is located 
-app.get('/', (req , res) => {
-    res.send('Hello World!');
-})
+const PORT = 3000;
+//creating the varibale of port and assigning it the value of 3000
 
-app.listen(port, () => {
-    console.log("Server is running on https://localhost:3000")
-})
+app.get("/", (req, res) => {
+  res.json("Hello World");
+  console.log("json sent");
+});
 
-//landing 'page' it runs wiht welcome message "Hello World!"
+app.get("/person/:hello/:world", (req, res) => {
+  const { hello, world } = req.params;
+  res.json({ hello, world });
+});
 
+app.get("/test/:input/:output", (req, res) => {
+  const { input, output } = req.params;
+  res.json({ input, output });
+});
 
+//telling our app (which is express), when it receives a get request, to the / url, to responp by sending ('Hello World!')
 
+app.listen(PORT, () => {
+  console.log("This app is running on PORT 3000");
+});
 
-//port on the server that listens for requests 
+//telling our app, which is express, to listen on the port varibale which we assigned, and when it is listening, to log the statment (`Example app listening on port ${port}`)
